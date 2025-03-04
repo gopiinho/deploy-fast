a40f06453fd000356b5316f9b8463d504d2aa4c4
Gurpreet <gs109690@gmail.com>
2025-03-04 18:49:54 +0530

    update contracts with manager

--- contracts/src/DeployFastManager.sol
+++ contracts/src/DeployFastManager.sol
@@ -1,6 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-contract DeployFastManager {
+import {ERC20Factory} from "./factory/ERC20Factory.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
+contract DeployFastManager is ERC20Factory, Ownable {
+    ERC20Factory erc20Factory;
+
+    constructor() Ownable(msg.sender) {
+        erc20Factory = new ERC20Factory();
+    }
+
+    function updateErc20Factory(address _erc20Factory) external onlyOwner {
+        erc20Factory = ERC20Factory(_erc20Factory);
+    }   
  }
--- contracts/src/factory/ERC20Factory.sol
+++ contracts/src/factory/ERC20Factory.sol
@@ -2,14 +2,13 @@
 pragma solidity 0.8.28;
 
 import {ERC20Contract} from "../tokens/ERC20.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ERC20Factory is Ownable {
+contract ERC20Factory  {
     ERC20Contract public erc20;
 
-    constructor() Ownable(msg.sender) {}
+    constructor() {}
 
-    function deployErc20(string memory name, string memory symbol, uint256 mintAmount, address mintTo) external onlyOwner returns(address) {
+    function _deployErc20(string memory name, string memory symbol, uint256 mintAmount, address mintTo) external returns(address) {
         address erc20Address = address(new ERC20Contract(name, symbol, mintAmount, mintTo));
         erc20 = ERC20Contract(erc20Address);
         return erc20Address;
