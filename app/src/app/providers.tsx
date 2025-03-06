a367317c7b5fa424449e2a76b4d56e76c1d2c871
Gurpreet <gs109690@gmail.com>
2025-03-06 15:31:18 +0530

    convex database

--- app/src/app/providers.tsx
+++ app/src/app/providers.tsx
@@ -4,16 +4,21 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WagmiProvider } from '@privy-io/wagmi'
 import { wagmiConfig } from '@/config/wagmiConfig'
 import { privyConfig } from '@/config/privyConfig'
+/* Backend */
+import { ConvexProvider, ConvexReactClient } from 'convex/react'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient()
+  const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
   return (
     <PrivyProvider
       appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ''}
       config={privyConfig}
     >
       <QueryClientProvider client={queryClient}>
-        <WagmiProvider config={wagmiConfig}>{children}</WagmiProvider>
+        <WagmiProvider config={wagmiConfig}>
+          <ConvexProvider client={convex}>{children}</ConvexProvider>
+        </WagmiProvider>
       </QueryClientProvider>
     </PrivyProvider>
   )
