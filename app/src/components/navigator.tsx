e5a25f32bf151da7998ce71136c33ad9518fb466
Gurpreet <gs109690@gmail.com>
2025-03-06 14:51:29 +0530

    text format and logic fix

--- app/src/components/navigator.tsx
+++ app/src/components/navigator.tsx
@@ -1,30 +1,32 @@
 'use client'
 import { usePrivy } from '@privy-io/react-auth'
+import { Button } from './ui/button'
 
 export default function Navigator() {
   const { ready, authenticated, login, logout } = usePrivy()
-  const disable = !ready || (ready && authenticated)
+  const disableLogin = !ready || (ready && authenticated)
+  const disableLogout = !ready || (ready && !authenticated)
 
   return (
     <section className="my-auto flex h-16 items-center justify-between px-10 text-sm">
       <h1>DeployFast</h1>
       <div className="flex gap-2">
         {ready && authenticated ? (
-          <button
-            disabled={disable}
+          <Button
+            disabled={disableLogout}
             onClick={logout}
             className="bg-foreground/10 text-foreground border-accent cursor-pointer rounded-3xl px-6 py-2 font-medium"
           >
             Log out
-          </button>
+          </Button>
         ) : (
-          <button
-            disabled={disable}
+          <Button
+            disabled={disableLogin}
             onClick={login}
             className="bg-foreground text-background border-accent cursor-pointer rounded-3xl px-6 py-2 font-medium"
           >
             Login
-          </button>
+          </Button>
         )}
       </div>
     </section>
