3a86415077d8e99d75c7c72fcbdd2c7ca31e0e11
Gurpreet <gs109690@gmail.com>
2025-03-24 00:14:45 +0530

    homepage redirect when authenticated

--- app/src/components/navbar.tsx
+++ app/src/components/navbar.tsx
@@ -12,9 +12,15 @@ export default function Navigator() {
 
   return (
     <section className="border-primary-foreground my-auto flex h-16 items-center justify-between border-b px-4 text-base sm:h-20 lg:px-6">
-      <Link href={'/'}>
-        <h2 className="text-xl font-semibold">DeployFast</h2>
-      </Link>
+      {ready && authenticated ? (
+        <Link href={'/contracts'}>
+          <h2 className="text-xl font-semibold">DeployFast</h2>
+        </Link>
+      ) : (
+        <Link href={'/'}>
+          <h2 className="text-xl font-semibold">DeployFast</h2>
+        </Link>
+      )}
       <div className="flex gap-2">
         {ready && authenticated ? (
           <NavProfile />
