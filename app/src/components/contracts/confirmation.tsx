34723fed93ffe2c166b5d7266a931faa3d340bb4
Gurpreet <gs109690@gmail.com>
2025-03-25 01:01:38 +0530

    token transaction confirmation screen

--- app/src/components/contracts/confirmation.tsx
+++ app/src/components/contracts/confirmation.tsx
@@ -1,5 +1,6 @@
 import { IoMdClose } from 'react-icons/io'
 import { MdUpload } from 'react-icons/md'
+import { MoonLoader } from 'react-spinners'
 import { Button } from '../ui/button'
 import { shortenAddress } from '@/lib/helpers'
 
@@ -10,6 +11,8 @@ interface ConfirmationProps {
   tokenSymbol?: string
   mintAmount?: number
   recipient?: string
+  close?: () => void
+  isLoading?: boolean
 }
 
 interface InputValueProps {
@@ -32,22 +35,33 @@ function InputValue({ title, value, isAddress }: InputValueProps) {
 }
 
 export default function Confirmation({
-  onClick,
   isToken,
   tokenName,
   tokenSymbol,
   mintAmount,
   recipient,
+  close,
+  isLoading,
 }: ConfirmationProps) {
   if (isToken)
     return (
-      <div className="bg-background/90 fixed inset-0 z-50 flex items-center justify-center">
-        <div className="border-border/30 rounded-lg border p-3 bg-blend-saturation backdrop-blur-md max-sm:w-full sm:min-w-60">
+      <div
+        className="bg-background/90 fixed inset-0 z-50 flex items-center justify-center"
+        onClick={close}
+      >
+        <div
+          className="border-border/30 rounded-lg border p-3 bg-blend-saturation backdrop-blur-md max-sm:w-full sm:min-w-60"
+          onClick={(e) => e.stopPropagation()}
+        >
           <div className="bg-background border-border/40 min-w-120 flex flex-col gap-4 rounded-lg border p-3 max-sm:w-full">
             <div className="flex items-center justify-between pb-2">
               <span></span>
               <span className="text-lg font-semibold">Review Transaction</span>
-              <IoMdClose size={20} />
+              <IoMdClose
+                size={20}
+                onClick={close}
+                className="cursor-pointer duration-150 hover:opacity-50"
+              />
             </div>
             <div className="space-y-4">
               <InputValue title="Token Name" value={tokenName} />
@@ -56,8 +70,14 @@ export default function Confirmation({
               <InputValue title="Recipient" value={recipient} isAddress />
             </div>
             <div className="mt-4 flex w-full justify-between space-x-2">
-              <Button size={'full'} onClick={onClick}>
-                Deploy Now <MdUpload />
+              <Button size={'full'} type="submit">
+                {isLoading ? (
+                  <MoonLoader size={20} />
+                ) : (
+                  <>
+                    Deploy Now <MdUpload />
+                  </>
+                )}
               </Button>
             </div>
           </div>
--- app/src/state/erc20FormStore.ts
+++ app/src/state/erc20FormStore.ts
@@ -6,11 +6,15 @@ type Erc20FormType = {
   description?: string
   mintAmount: number
   recipient: string
+  confirming: boolean
+  loading: boolean
   setName: (name: string) => void
   setSymbol: (symbol: string) => void
   setDescription: (description: string) => void
   setMintAmount: (mintAmount: number) => void
   setRecipient: (recipient: string) => void
+  setConfirming: (confirming: boolean) => void
+  setLoading: (loading: boolean) => void
 }
 
 const useErc20FormStore = create<Erc20FormType>((set) => ({
@@ -19,11 +23,15 @@ const useErc20FormStore = create<Erc20FormType>((set) => ({
   description: '',
   mintAmount: 0,
   recipient: '',
+  confirming: false,
+  loading: false,
   setName: (name) => set(() => ({ name })),
   setSymbol: (symbol) => set(() => ({ symbol })),
   setDescription: (description) => set(() => ({ description })),
   setMintAmount: (mintAmount) => set(() => ({ mintAmount })),
   setRecipient: (recipient) => set(() => ({ recipient })),
+  setConfirming: (confirming) => set(() => ({ confirming })),
+  setLoading: (loading) => set(() => ({ loading })),
 }))
 
 export default useErc20FormStore
