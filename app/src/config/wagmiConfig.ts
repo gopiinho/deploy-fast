d8fe2516b0e242e7eef8770fd75bda441ef1736e
Gurpreet <gs109690@gmail.com>
2025-05-06 21:18:58 +0530

    ✨ Added multiple chains

--- app/src/config/wagmiConfig.ts
+++ app/src/config/wagmiConfig.ts
@@ -1,11 +1,16 @@
-import { baseSepolia } from 'viem/chains'
+import { mainnet, base, optimism, arbitrum, baseSepolia } from 'viem/chains'
 import { http } from 'wagmi'
 
 import { createConfig } from '@privy-io/wagmi'
+import { chains, chainsTestnet } from '@/lib/chains'
 
 export const wagmiConfig = createConfig({
-  chains: [baseSepolia],
+  chains: [mainnet, base, optimism, arbitrum, baseSepolia],
   transports: {
-    [baseSepolia.id]: http(),
+    [mainnet.id]: http(chains.ethereum.rpcUrl),
+    [base.id]: http(chains.base.rpcUrl),
+    [optimism.id]: http(chains.optimism.rpcUrl),
+    [arbitrum.id]: http(chains.arbitrum.rpcUrl),
+    [baseSepolia.id]: http(chainsTestnet.baseSepolia.rpcUrl),
   },
 })
