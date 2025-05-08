ef31dc6353086e7b33cf57350e849c0baa6d6007
Gurpreet <gs109690@gmail.com>
2025-05-08 20:06:15 +0530

    🔧 Update the factory address, abi and standard-input-json for etherscan

--- app/src/lib/constants.ts
+++ app/src/lib/constants.ts
@@ -1,6 +1,6 @@
 import { Address } from 'viem'
 
-const DF_MANAGER: Address = '0xABeD8581ebAdC5E95110E6Be61cEB51af343fBFd'
+const DF_MANAGER: Address = '0xEB49134C3185aC1Ea9C5c170ea425B0De7Df4071'
 
 const ETHERSCAN_API_VERIFY_URL: string = `https://api-sepolia.basescan.org/api`
 
